225f48209e6dafe2fe388906b7cf226ddde1c713
dhsc <1364125913@qq.com>
2017-09-13 06:40:15 +0800

    删除响应代码，改用public.js

--- touchwap/js/weiyilai.js
+++ touchwap/js/weiyilai.js
@@ -2,11 +2,6 @@ $(function(){
     var animateCurve = $('.animate-curve'),
         animateCurveHigh = (212/276)*animateCurve.width(),
         initWidth = 0,
-/*start 与320相比，大于320时，每差20px则font-size加0.5，小于320时，font-size=为10px*/
-        vwDVal = $('.path01').width() - 320,
-        initFontSize = vwDVal>0 ? 12 + (Math.floor(vwDVal/20)/2) : 10;
-    $('html').css({fontSize: initFontSize+'px'});
-/*end*/
     animateCurve.height(animateCurveHigh);
     animateCurve.width('0');
     animateCurve.animate({width: '95%'},2000);
